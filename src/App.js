698ff37f2add964dc18542483afb4a9f222cc19d
max petrusenko <max_p@maxpetrusenko-2.local>
2020-06-12 21:46:11 -0700

    nasaphoto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import DatePicker from "react-date-picker"
 const axios = require('axios').default;
 
 function App() {
-  const [query,setQuery] = useState([])
+  let [query,setQuery] = useState([])
   const [d, changeD] = useState(new Date())
   function changeDate(date){
     changeD(date)
@@ -17,16 +17,28 @@ const dateTimeFormat = new Intl.DateTimeFormat('en', { year: 'numeric', month: '
 const [{ value: month },,{ value: day },,{ value: year }] = dateTimeFormat.formatToParts(d) 
 // console.log(`${day}-${month}-${year }`)
 
-  useEffect(()=> {
-    axios.get('https://api.nasa.gov/planetary/apod?api_key=ykjZwlovnAtPc6qHMBmRY4uD8tm1fIu9g7JTVLgy' + "&date=" +`${year}-${month}-${day}`)
-        .then(function(response) {
-          console.log(response)
-          setQuery(response.data)
-      })
-      .catch(function(error){
-        console.log(error)
-    })
-  }, [d])
+  // useEffect(()=> {
+  //   axios.get('https://api.nasa.gov/planetary/apod?api_key=ykjZwlovnAtPc6qHMBmRY4uD8tm1fIu9g7JTVLgy' + "&date=" +`${year}-${month}-${day}`)
+  //       .then(function(response) {
+  //         console.log(response)
+  //         setQuery(response.data)
+  //     })
+  //     .catch(function(error){
+  //       console.log(error)
+  //   })
+  // }, [d])
+  
+query = { 
+      date: "2020-06-11",
+      explanation: "Big, beautiful, barred spiral galaxy NGC 1300 lies some 70 million light-years away on the banks of the constellation Eridanus. This Hubble Space Telescope composite view of the gorgeous island universe is one of the largest Hubble images ever made of a complete galaxy. NGC 1300 spans over 100,000 light-years and the Hubble image reveals striking details of the galaxy's dominant central bar and majestic spiral arms. In fact, on close inspection the nucleus of this classic barred spiral itself shows a remarkable region of spiral structure about 3,000 light-years across. Like other spiral galaxies, including our own Milky Way, NGC 1300 is thought to have a supermassive central black hole.",
+      hdurl: "https://apod.nasa.gov/apod/image/2006/NGC1300HSTfull.jpg",
+      media_type: "image",
+      service_version: "v1",
+      title: "Barred Spiral Galaxy NGC 1300",
+      url: "https://apod.nasa.gov/apod/image/2006/NGC1300HST1200.jpg",
+
+    }
+  
 
   return (
     <div className="App">
--- src/components/Component.js
+++ src/components/Component.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import styled from 'styled-components';
 
+const WrapperH1 = styled.div`
+    color: red;
+
+    ${props => (props.type === 'primary'? `background:#2196f3;` : null)}
+    ${props => (props.type === 'success'? `background:#4caf50;` : null)}
+    ${props => (props.type === 'danger' ? `background:#f44336;` : null)}
+    ${props => (props.type === 'warning'? `background:#fdd835;` : null)}
+`
 
 function Component({copywrite,date,explanation,url,title}) {
 
     return (
     <div className='main'>
     <div className='container'>
+        <h1 className="title">Title: {title}</h1>
+        <WrapperH1 type='primary'>Title: {title}</WrapperH1>
         <img className='imag' src={url}></img>
         <div className='text-container '>
-        <span></span>
-        <h1 className="title">Title: {title}</h1>
-        <p>Date: {date}</p>
-        <p>Explanation: {explanation}</p>
-        <p>writer: {copywrite}</p>
+            <span></span>
+            <p>Date: {date}</p>
+            <p>Explanation: {explanation}</p>
+            <p>writer: {copywrite}</p>
         </div>
     </div>
     </div>
